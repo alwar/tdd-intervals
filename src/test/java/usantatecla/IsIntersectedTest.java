6bda02195a1e2eb5f9a5dd01ba7fdf3ed1f48442
Alvaro Guzman <aguzman@hn-services.com>
2020-11-30 00:42:48 +0100

    red given Min Is Included And Max Is Excluded Then isIntersected Return False

--- src/test/java/usantatecla/IsIntersectedTest.java
+++ src/test/java/usantatecla/IsIntersectedTest.java
@@ -7,6 +7,13 @@ import org.junit.jupiter.api.Test;
 
 public class IsIntersectedTest {
 
+    // si interseccion -> afirmativo
+    // si no hay interseccion -> negativo
+    // min inc. max inc. true
+    // min inc. max exc. false
+    // min exc. max inc. false
+    // min exc. max exc. false
+
     @Test
     public void givenAnIntervalIntersectAnotherIntervalReturnTrue() {
 
@@ -22,6 +29,24 @@ public class IsIntersectedTest {
         Interval sut = new Interval(new Min(1), new Max(4));
 
         boolean result = sut.isIntersected(new Interval(new Min(5), new Max(7)));
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void givenMinIsIncludedAndMaxIsIncludedThenIsIntersectedReturnTrue() {
+        Interval sut = new Interval(new ClosedMin(1), new ClosedMax(4));
+
+        boolean result = sut.isIntersected(new Interval(new ClosedMin(1), new ClosedMax(4)));
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void givenMinIsIncludedAndMaxIsExcludedThenIsIntersectedReturnFalse() {
+        Interval sut = new Interval(new ClosedMin(1), new ClosedMax(4));
+
+        boolean result = sut.isIntersected(new Interval(new ClosedMin(1), new ClosedMax(5)));
 
         assertFalse(result);
     }

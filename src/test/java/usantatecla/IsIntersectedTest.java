4c13ef2091198dfc1bc44a6736351d390192a09a
Alvaro Guzman <aguzman@hn-services.com>
2020-11-30 00:13:57 +0100

    red given An Interval Doesnt Intersect Another Interval Return False

--- src/test/java/usantatecla/IsIntersectedTest.java
+++ src/test/java/usantatecla/IsIntersectedTest.java
@@ -1,5 +1,6 @@
 package usantatecla;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -14,5 +15,14 @@ public class IsIntersectedTest {
         boolean result = sut.isIntersected(new Interval(new Min(2), new Max(3)));
 
         assertTrue(result);
+    }
+
+    @Test
+    public void givenAnIntervalDoesntIntersectAnotherIntervalReturnFalse() {
+        Interval sut = new Interval(new Min(1), new Max(4));
+
+        boolean result = sut.isIntersected(new Interval(new Min(5), new Max(7)));
+
+        assertFalse(result);
     }
 }
